ddad33262a249003582a3eb9490a718c422d8f63
MunirZ <munirzohbi@gmail.com>
2023-01-11 11:30:51 +0100

    Titel, Anlegen-Button und Mitarbeiterliste hinzugefügt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import{EmployeeListComponent} from "./employee-list/employee-list.component";
+import{ EmployeeListComponent } from "./employee-list/employee-list.component";
 import { LoginComponent } from './login/login.component';
+import { PopupComponent } from './popup/popup.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/employee', pathMatch: 'full' },
   { path: 'employee', component: EmployeeListComponent},
+  { path: 'popup', component: PopupComponent},
   { path: 'login', component: LoginComponent}
 ];
 
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -11,13 +11,21 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 export class EmployeeListComponent {
 
   bearer = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE2NzI3NjgxMDgsImlhdCI6MTY3Mjc2NDUwOCwianRpIjoiNGIyYmIwYjMtOTFiZS00MGRjLTkyMjQtNmMxY2ZlYzJiZTc2IiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiIxN2I4MmNlYi1mZDQ3LTQzYjAtODBlYy0xNTRlYzk5N2VhNDMiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.RwEfHQDiwzTXlvSma4HnRLMWpYORppIdugZFpJ1RpGIvT-xvY2vc6HlMjsIs36euYCMTZOd51x1Y8EJATEfzXiYNtef980DGlIbnX_vhLirA08FVJnkDSHt1YHfEjhYOMOY9DrsuIFkim80NaPPb1aNVFQ1PNNw_CZ7TLsnrPoO0oXCqKMH2tLUo-SgTablRsK30qJ4HzP58AHkmr0fyqSbwbu1il73ngxJ4JCfKgtYm8-F8O2KbHECOom9Q5O5ZSYPDKCNiVylbzrtdHGFpmOvgq5ZURIQk9qDexKcaEEoOr8YiIjDbqh1zNF-OpIeN8PnU2WFyU3XuaTQihjZDbg';
-  employees$: Observable<Employee[]>;
+  //employees$: Observable<Employee[]> = {};
+
+  employees$: Employee[] = [
+    new Employee(0,"Ben","Stierbeiner","Grenzweg 1","27798","Hude","01627684680"),
+    new Employee(0,"Ben","Stierbeiner","Grenzweg 1","27798","Hude","01627684680"),
+    new Employee(0,"Ben","Stierbeiner","Grenzweg 1","27798","Hude","01627684680"),
+    new Employee(0,"Ben","Stierbeiner","Grenzweg 1","27798","Hude","01627684680")
+  ];
 
   constructor(private http: HttpClient) {
-    this.employees$ = of([]);
-    this.fetchData();
+    //this.employees$ = of([]);
+    //this.fetchData();
   }
 
+  /*
   fetchData() {
     this.employees$ = this.http.get<Employee[]>('/backend', {
       headers: new HttpHeaders()
@@ -25,5 +33,5 @@ export class EmployeeListComponent {
         .set('Authorization', `Bearer ${this.bearer}`)
     });
   }
-
+  */
 }
