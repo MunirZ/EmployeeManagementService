99aa7cd03dc430cbab0055f4f1659dc24a929c77
MunirZ <munirzohbi@gmail.com>
2023-01-11 11:11:51 +0100

    Popup und Login component hinzugefügt
    Co-authored-by: Tim <derTunnel@users.noreply.github.com>
    Co-authored-by: jonasChristoph <jonasChristoph@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { LoginComponent } from './login/login.component';
+import { PopupComponent } from './popup/popup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    LoginComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
