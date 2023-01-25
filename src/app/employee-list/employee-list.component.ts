7d03816b4c42bf0a9dfa2be8e10fbc18def93450
MunirZ <munirzohbi@gmail.com>
2023-01-25 11:35:38 +0100

    Datenbankaufrufe hinzugefügt

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -19,7 +19,7 @@ export class EmployeeListComponent {
   isAddressClicked: boolean = false;
 
   bearer = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE2NzI3NjgxMDgsImlhdCI6MTY3Mjc2NDUwOCwianRpIjoiNGIyYmIwYjMtOTFiZS00MGRjLTkyMjQtNmMxY2ZlYzJiZTc2IiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiIxN2I4MmNlYi1mZDQ3LTQzYjAtODBlYy0xNTRlYzk5N2VhNDMiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.RwEfHQDiwzTXlvSma4HnRLMWpYORppIdugZFpJ1RpGIvT-xvY2vc6HlMjsIs36euYCMTZOd51x1Y8EJATEfzXiYNtef980DGlIbnX_vhLirA08FVJnkDSHt1YHfEjhYOMOY9DrsuIFkim80NaPPb1aNVFQ1PNNw_CZ7TLsnrPoO0oXCqKMH2tLUo-SgTablRsK30qJ4HzP58AHkmr0fyqSbwbu1il73ngxJ4JCfKgtYm8-F8O2KbHECOom9Q5O5ZSYPDKCNiVylbzrtdHGFpmOvgq5ZURIQk9qDexKcaEEoOr8YiIjDbqh1zNF-OpIeN8PnU2WFyU3XuaTQihjZDbg';
-  //employees$: Observable<Employee[]> = {};
+  employeesDB$: Observable<Employee[]>;
 
   employees$: Employee[] = [
     new Employee(0, "atierbeiner", "Ben", "Grenzweg 1", "27798", "Hude", "01627684681"),
@@ -36,8 +36,9 @@ export class EmployeeListComponent {
   ];
 
   constructor(private http: HttpClient) {
-    //this.employees$ = of([]);
-    //this.fetchData();
+    this.employeesDB$ = of([]);
+    this.addEmployee();
+    this.fetchData();
   }
 
   @ViewChild("employeePopup") employeePopup!: PopupComponent;
@@ -121,13 +122,28 @@ export class EmployeeListComponent {
 
   }
 
-  /*
   fetchData() {
-    this.employees$ = this.http.get<Employee[]>('/backend', {
+    this.employeesDB$ = this.http.get<Employee[]>('http://localhost:8089/backend', {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`)
     });
+    console.log(this.employeesDB$);
+  } 
+
+  addEmployee() {
+    const body = { "lastName":  "Freddy",
+                   "firstName": "Krueger",
+                   "street":    "nightmare on elm street ",
+                   "postcode":  "00000",
+                   "city":      "Arkham",
+                   "phone":     "932849233" }
+
+    this.http.put<any>('http://localhost:8089/backend', body, { 
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${this.bearer}`) }).subscribe(data => {
+          console.log(data)});
   }
-  */
 }
+
